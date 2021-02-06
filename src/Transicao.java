f9c5df0bff4b2fb285450c20e5adeb913fcf58f9
Augusto Lima <augustocesarld@hotmail.com>
2021-02-06 10:00:45 -0300

    Add JavaDoc

--- src/Transicao.java
+++ src/Transicao.java
@@ -2,6 +2,15 @@ public class Transicao {
     private final int estadoOrigem, estadoDestino;
     private final char valorInicial, valorFinal, direcao;
     
+    /**
+     * Construtor da Classe Transicao
+     * 
+     * @param estadoOrigem
+     * @param estadoDestino
+     * @param valorInicial
+     * @param valorFinal
+     * @param direcao
+     */
     public Transicao(int estadoOrigem, int estadoDestino, char valorInicial, char valorFinal, char direcao) {
         this.estadoOrigem = estadoOrigem;
         this.estadoDestino = estadoDestino;
@@ -10,22 +19,47 @@ public class Transicao {
         this.direcao = direcao;
     }
 
+    /**
+     * Retorna o estado de origem
+     * 
+     * @return
+     */
     public int getEstadoOrigem() {
         return estadoOrigem;
     }
 
+    /**
+     * Retorna o estado de destino
+     * 
+     * @return
+     */
     public int getEstadoDestino() {
         return estadoDestino;
     }
 
+    /**
+     * Retorna o valor inicial
+     * 
+     * @return
+     */
     public char getValorInicial() {
         return valorInicial;
     }
 
+    /**
+     * Retorna o valor final
+     * 
+     * @return
+     */
     public char getValorFinal() {
         return valorFinal;
     }
 
+    /**
+     * Retorna a direção da cabeça de leitura
+     * 
+     * @return
+     */
     public char getDirecao() {
         return direcao;
     }
